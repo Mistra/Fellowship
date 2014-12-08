1f886d42a6409e6a6df2864513f2171a92bbfbd1
Giulio Mistrangelo <sperectrum@gmail.com>
2014-12-08 03:23:36 +0100

    forgot to add controller to previous commit

---
+++ controller/login.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace app\controller;
+
+class login {
+  function __construct() {
+    require_once ("view/login/login.php");
+    $this->view = new \app\view\login;
+  }
+  function index() {
+    $this->view->index();
+  }
+
+  function loginUser() {
+      $id = filter_input(INPUT_POST, 'id');
+      require_once("model/domainRepository.php");
+      require_once("model/autenticator.php");
+      $userCollection = new \app\model\repository("user");
+      $user = $userCollection->retrive($id);
+
+      \app\model\autenticator::login($user);
+
+      header("location: http://fellowship/login");
+  }
+
+  function logoutUser() {
+
+      require_once("model/autenticator.php");
+      \app\model\autenticator::logout();
+      header("location: http://fellowship/login");
+  }
+}
